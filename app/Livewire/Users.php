862b64675e94fd5ed080a8247a0b2b413c1c9fdf
l-yazan-l <103342472+l-yazan-l@users.noreply.github.com>
2024-01-07 23:31:43 +0300

    main

---
+++ app/Livewire/Users.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\User;
+use Livewire\Component;
+
+class Users extends Component
+{
+    public $users; 
+    
+    public $formMode = null;
+    public $UserId;
+    public $name;
+    public $email;
+    public $password;
+    
+    public function cancelBox()
+    {
+        $this->UserId = '';
+        $this->name = '';
+        $this->email = '';
+    }
+    public function index()
+    {
+        return view('dashboard');
+    } 
+
+    public function mount()
+    {
+        $this->users = User::all();
+    }
+    public function render()
+    {
+        return view('livewire.users');
+    }
+    public function editUser($userId)
+    {
+        $user = User::findOrFail($userId);
+        $this->UserId = $user->id;
+        $this->name = $user->name;
+        $this->email = $user->email;
+
+    }
+    public function updateUser()
+    {
+        $user = User::findOrFail($this->UserId);
+        $user->update([
+            'name' => $this->name,
+            'email' => $this->email,
+        ]);
+        session()->flash('message', 'User updated successfully!');
+        return redirect('/dashboard');
+    }
+    public function destroyUser($UserId)
+    {
+        $user = User::find($UserId);
+        $user->delete();
+        session()->flash('message', 'User deleted successfully!');
+        return redirect('/dashboard');
+
+    }
+    public function createUser()
+    {
+        $newUser = User::create([
+            'name' => $this->name,
+            'email' => $this->email,
+            'password' => bcrypt($this->password),
+        ]);
+        session()->flash('message', 'User created successfully!');
+        return redirect('/dashboard');
+        
+    }
+      
+    
+}
---
+++ resources/views/livewire/users.blade.php
@@ -0,0 +1,63 @@
+<div>
+    <table class="table">
+        <thead>
+            <tr class="text-center">
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Email</th>
+                <th scope="col"> Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($users as $user)
+            <tr class="text-center">
+                <td>{{ $user->id }}</td>
+                <td>{{ $user->name }}</td>
+                <td>{{ $user->email }}</td>
+                <td><button wire:click="editUser({{ $user->id }})" class="text-info me-4">Edit</button>
+                    <button wire:click="destroyUser({{ $user->id }})" class="text-danger">Delete</button></td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+    @if($UserId)
+    <div class="edit-form">
+        <div class="form-floating mb-3">
+            <input type="text" wire:model="name" class="form-control" placeholder="Name">
+            <label for="name">Name</label>
+        </div>
+        <div class="form-floating mb-3">
+            <input type="email" wire:model="email" class="form-control" placeholder="name@example.com">
+            <label for="email">Email address</label>
+        </div>
+        <button wire:click="updateUser" class="btn btn-primary btn-sm">Update</button>
+        <button wire:click="cancelBox" class="btn btn-secondary btn-sm">Cancel</button>
+    </div>
+    @endif
+
+    @if($formMode === 'create')
+    <div class="update-form">
+        <div class="form-floating mb-3">
+            <input type="text" wire:model="name" class="form-control" placeholder="Name">
+            <label for="name">Name</label>
+        </div>
+        <div class="form-floating mb-3">
+            <input type="email" wire:model="email" class="form-control" placeholder="name@example.com">
+            <label for="email">Email address</label>
+        </div>
+        <div class="form-floating mb-3">
+            <input type="password" wire:model="password" class="form-control" placeholder="Enter the password">
+            <label for="email">Password</label>
+        </div>
+        <button wire:click="createUser" class="btn btn-success btn-sm">submit</button>
+        <button wire:click="cancelBox" class="btn btn-secondary btn-sm">Cancel</button>
+    </div>
+    @endif
+    @if (session()->has('message'))
+    <div class="container mt-4">
+        <div class="alert alert-success">
+            {{ session('message') }}
+        </div>
+    </div>
+    @endif
+</div>
