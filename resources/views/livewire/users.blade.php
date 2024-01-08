a4ee0b0b7a7624f1f84526b968e7ec479fd106f4
l-yazan-l <103342472+l-yazan-l@users.noreply.github.com>
2024-01-08 18:28:13 +0300

    test3

--- resources/views/livewire/users.blade.php
+++ resources/views/livewire/users.blade.php
@@ -1,5 +1,9 @@
 <div>
-    <button wire:click ="toggleForm" class="btn btn-md btn-success text-end">Create</button>
+    <div class="d-flex justify-content-end">
+        <svg wire:click="toggleForm" xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="green" class="bi bi-plus text-end" viewBox="0 0 16 16">
+            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4"/>
+        </svg>
+    </div>
     <table class="table">
         <thead>
             <tr class="text-center">
