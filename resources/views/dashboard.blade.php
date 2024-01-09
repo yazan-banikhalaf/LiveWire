0392dcf17196185d52e6d54941d39fb414eb0ed6
l-yazan-l <103342472+l-yazan-l@users.noreply.github.com>
2024-01-09 18:47:47 +0300

    Adding search bar

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <title>CRUD</title>
-    
+
 </head>
 <body>
 
@@ -44,6 +44,7 @@
             </div>
         </div>
     </x-app-layout>
+    @livewireScripts
 </body>
 </html>
 
